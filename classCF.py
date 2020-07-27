a0e1cfc3533b6faf2fce7ef9f0f6535be56e1760
minhtran1397 <minhtran1397@gmail.com>
2020-07-27 19:16:21 +0700

    COS, COR, CPC

--- classCF.py
+++ classCF.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np  # xử lý mảng
 from scipy import sparse  # spare: thưa thớt, chuẩn hóa ma trận
+import math
+from math import *
 # thư viện thuật toán (có độ tương đồng với gì nữa á)
-from sklearn.metrics.pairwise import cosine_similarity
+# from sklearn.metrics.pairwise import cosine_similarity
 # from sklearn.metrics import jaccard_score
 '''
 From scikit - learn: [‘cityblock’, ‘cosine’, ‘euclidean’, ‘l1’, ‘l2’, ‘manhattan’].
@@ -17,7 +19,7 @@ From scipy.spatial.distance: [‘braycurtis’, ‘canberra’, ‘chebyshev’,
 These metrics do not support sparse matrix inputs.
 '''
 # from scipy.spatial.distance import jaccard
-from scipy.spatial.distance import correlation
+# from scipy.spatial.distance import correlation
 # from scipy.spatial import distance
 
 
@@ -59,7 +61,69 @@ def msd_similarity(u, v, l):
     common_arr = common_dimensions(u, v)
     msd = sum(pow(a - b, 2) for a, b in zip(common_arr[0], common_arr[1]))
     intersection_cardinality = len(common_arr[0])
-    return (1-((msd/intersection_cardinality)/l))
+    if intersection_cardinality == 0:
+        return 0
+    else:
+        return (1-((msd/intersection_cardinality)/l))
+
+
+def square_rooted(x, y=0):
+    """ return 3 rounded square rooted value """
+    return round(sqrt(sum([(a - y) * (a - y) for a in x])), 3)
+
+
+def cos_similarity(u, v):
+    common_arr = common_dimensions(u, v)
+    common_u = common_arr[0]
+    common_v = common_arr[1]
+    numerator = sum(a * b for a, b in zip(common_u, common_v))
+    denominator = square_rooted(
+        common_u)*square_rooted(common_v)
+    if denominator == 0:
+        return 0
+    else:
+        return round(numerator / float(denominator), 3)
+
+
+def average(my_list):
+    if len(my_list) == 0:
+        return 0
+    else:
+        avg = float(sum(my_list)) / float(len(my_list))
+        return avg
+
+
+def cor_similarity(u, v):
+    # rm: lấy average(common):
+    common_arr = common_dimensions(u, v)
+    common_u = common_arr[0]
+    common_v = common_arr[1]
+    avg_u = average(common_u)
+    avg_v = average(common_v)
+    numerator = sum((a - avg_u)*(b - avg_v)
+                    for a, b in zip(common_u, common_v))
+    denominator = square_rooted(
+        common_u, avg_u)*square_rooted(common_v, avg_v)
+    if denominator == 0:
+        return 0
+    else:
+        return round(numerator / float(denominator), 3)
+
+
+def cpc_similarity(u, v):
+    # rm: lấy average(common):
+    rm = 2.5
+    common_arr = common_dimensions(u, v)
+    common_u = common_arr[0]
+    common_v = common_arr[1]
+    numerator = sum((a - rm)*(b - rm)
+                    for a, b in zip(common_u, common_v))
+    denominator = square_rooted(
+        common_u, rm)*square_rooted(common_v, rm)
+    if denominator == 0:
+        return 0
+    else:
+        return round(numerator / float(denominator), 3)
 
 
 class CF(object):
@@ -141,8 +205,29 @@ class CF(object):
 
     def similarity(self, method=1):
         if method == 1:  # cosine
-            # TODO: tìm .T là gì: .T là ma trận nghịch đảo, ở đây nghịch đảo vị trí user-item thành vị trí item-user
-            self.S = self.dist_func(self.Ybar.T, self.Ybar.T)
+            # T là ma trận nghịch đảo, ở đây nghịch đảo vị trí user-item thành vị trí item-user
+            # self.S = self.dist_func(self.Ybar.T, self.Ybar.T) #dùng thư viện
+            self.sim_cos = np.zeros((self.n_users, self.n_users))
+            # users = self.Y_data[:, 0]
+            # print(self.sim_cos)
+            print(self.n_users)
+            for i in range(self.n_users):
+                # ids = np.where(users == i)[0].astype(np.int32)
+                ratings_i_nD = self.Ybar.T[i, :].toarray()
+                # Xuất ra mảng nhiều chiều [[value]], cần truyền mảng 1 chiều [value] => :
+                ratings_i = ratings_i_nD[0]
+                print('u{}={}'.format(i, ratings_i))
+                for j in range(self.n_users):
+                    # jds = np.where(users == j)[0].astype(np.int32)
+                    ratings_j_nD = self.Ybar.T[j, :].toarray()
+                    # Xuất ra mảng nhiều chiều [[value]], cần truyền mảng 1 chiều [value] => :
+                    ratings_j = ratings_j_nD[0]
+                    print('v{}={}'.format(j, ratings_j))
+                    print("-----")
+                    self.sim_cos[i, j] = self.dist_func(
+                        ratings_i, ratings_j)
+                self.sim_cos[i]
+            self.S = self.sim_cos
         # jaccard np.where(a > 0.5, 1, 0) ý tưởng là dùng hàm trên id của item non zero
         if method == 2:
             self.sim_jaccard = np.zeros((self.n_users, self.n_users))
@@ -162,11 +247,64 @@ class CF(object):
                         item_ids, item_jds)
                 self.sim_jaccard[i]
             self.S = self.sim_jaccard
-        if method = 3:  # msd_similarity
-            # TODO:
+        if method == 3:  # msd_similarity
+            self.sim_msd = np.zeros((self.n_users, self.n_users))
+            # users = self.Y_data[:, 0]
+            print(self.sim_msd)
+            print(self.n_users)
+            for i in range(self.n_users):
+                # ids = np.where(users == i)[0].astype(np.int32)
+                ratings_i_nD = self.Ybar.T[i, :].toarray()
+                # Xuất ra mảng nhiều chiều [[value]], cần truyền mảng 1 chiều [value] => :
+                ratings_i = ratings_i_nD[0]
+                print('u{}={}'.format(i, ratings_i))
+                for j in range(self.n_users):
+                    # jds = np.where(users == j)[0].astype(np.int32)
+                    ratings_j_nD = self.Ybar.T[j, :].toarray()
+                    # Xuất ra mảng nhiều chiều [[value]], cần truyền mảng 1 chiều [value] => :
+                    ratings_j = ratings_j_nD[0]
+                    print('v{}={}'.format(j, ratings_j))
+                    print("-----")
+                    self.sim_msd[i, j] = self.dist_func(
+                        ratings_i, ratings_j, 25)
+                self.sim_msd[i]
+            self.S = self.sim_msd
+        if method == 4:  # COR
+            self.sim_cor = np.zeros((self.n_users, self.n_users))
+            print(self.n_users)
+            for i in range(self.n_users):
+                ratings_i_nD = self.Ybar.T[i, :].toarray()
+                ratings_i = ratings_i_nD[0]
+                print('u{}={}'.format(i, ratings_i))
+                for j in range(self.n_users):
+                    ratings_j_nD = self.Ybar.T[j, :].toarray()
+                    ratings_j = ratings_j_nD[0]
+                    print('v{}={}'.format(j, ratings_j))
+                    print("-----")
+                    self.sim_cor[i, j] = self.dist_func(
+                        ratings_i, ratings_j)
+                self.sim_cor[i]
+            self.S = self.sim_cor
+        if method == 5:  # CPC
+            self.sim_cpc = np.zeros((self.n_users, self.n_users))
+            print(self.n_users)
+            for i in range(self.n_users):
+                ratings_i_nD = self.Ybar.T[i, :].toarray()
+                ratings_i = ratings_i_nD[0]
+                print('u{}={}'.format(i, ratings_i))
+                for j in range(self.n_users):
+                    ratings_j_nD = self.Ybar.T[j, :].toarray()
+                    ratings_j = ratings_j_nD[0]
+                    print('v{}={}'.format(j, ratings_j))
+                    print("-----")
+                    self.sim_cpc[i, j] = self.dist_func(
+                        ratings_i, ratings_j)
+                self.sim_cpc[i]
+            self.S = self.sim_cpc
         print('S nè :')
         print(self.S)
-    # Thực hiện lại 2 hàm phía trên khi có thêm dữ liệu.
+
+    # Thực hiện lại 2 hàm phía trên khi có thêm dữ liệu:
 
     def refresh(self, method=1):
         """
--- main.py
+++ main.py
@@ -3,14 +3,19 @@ from classCF import *
 r_cols = ['user_id', 'item_id', 'rating']
 ratings = pd.read_csv('ex.dat', sep=' ', names=r_cols, encoding='latin-1')
 Y_data = ratings.to_numpy()  # as_matrix() không chạy
-COS = cosine_similarity  # method = 1
+COS = cos_similarity  # method = 1
 JAC = rjaccard_similarity  # method = 2
+MSD = msd_similarity  # method = 3
+COR = cor_similarity  # method = 4
+CPC = cpc_similarity  # method = 5
+
 #rs = CF(Y_data, k=2, dist_func=COS, uuCF=1)
 
-rs = CF(Y_data, k=2, dist_func=JAC, uuCF=1)
+rs = CF(Y_data, k=2, dist_func=CPC, uuCF=1)
 # rs.fit()
 print("############ Bắt đầu đầu đầu ####################")
-rs.fit(2)
+# fit(method)
+rs.fit(5)
 # rs.normalize_Y()
 # print(rs.similarity())
 print("xxxxxxxxxxxx Kết thúc xxxxxxxxxxxxxxxxxxxx")
