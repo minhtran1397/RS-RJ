643fa3ae8f9bef399b539cada2662d34e1b26676
ThanhNguyet <thanhnguyet.10toan@gmail.com>
2020-08-07 12:30:22 +0700

    test F1-score

--- runmovielens.py
+++ runmovielens.py
@@ -1,4 +1,8 @@
 from classCF import *
+from sklearn.metrics import f1_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import precision_recall_fscore_support
 r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
 
 ratings_base = pd.read_csv('ub.base', sep='\t',
@@ -6,7 +10,7 @@ ratings_base = pd.read_csv('ub.base', sep='\t',
 ratings_test = pd.read_csv('ub.test', sep='\t',
                            names=r_cols, encoding='latin-1')
 
-#rate_train = ratings_base.as_matrix()
+# rate_train = ratings_base.as_matrix()
 rate_train = ratings_base.to_numpy()
 
 rate_test = ratings_test.to_numpy()
@@ -23,21 +27,36 @@ COR = cor_similarity  # method = 3
 CPC = cpc_similarity  # method = 3
 RJ = rjaccard_similarity  # method = 2
 # METHOD 4 DÙNG CHO LOOP_USERZZZZZ
-rs = CF(rate_train, k=30, dist_func=COS_lib, uuCF=1)
+rs = CF(rate_train, k=20, dist_func=COS_lib, uuCF=1)
 # fit(method)
 rs.fit(1)
 
 n_tests = rate_test.shape[0]
 SE = 0  # squared error
+pred_arr = np.array([0])
+real_rate_arr = np.array([0])
 for n in range(n_tests):
     pred = rs.pred(rate_test[n, 0], rate_test[n, 1], normalized=1)
-    print("pred", pred)
-    print("rate_test[{}, 2]={}".format(n, rate_test[n, 2]))
-    SE += (pred - rate_test[n, 2]) ** 2
-
-RMSE = np.sqrt(SE/n_tests)
+    pred_arr = np.append(pred_arr, [pred], axis=0)
+    real_rate_arr = np.append(real_rate_arr, [rate_test[n, 2]], axis=0)
+    # print("pred", [pred])
+    # prireal_rate_arrnt("rate_test[{}, 2]={}".format(n, rate_test[n, 2]))
+    # Lưu mảng pred
+    # kt độ 9xacs trên mảng pred với mảng
+    # SE += (pred - rate_test[n, 2]) ** 2
+print("pred_arr", pred_arr.shape)
+print("real_rate_arr", real_rate_arr.shape)
+# RMSE = np.sqrt(SE/n_tests)
+RMSE = sqrt(((pred_arr-real_rate_arr)**2).sum()/n_tests)
 print('User-user CF, RMSE =', RMSE)
-
+# F1 = precision_recall_fscore_support( real_rate_arr, pred_arr.round(), average = 'macro', zero_division = 1)
+F1 = f1_score(real_rate_arr, pred_arr.round(),
+              average=None, zero_division=1)
+print("f1_score nè:", F1)
+rc = recall_score(real_rate_arr, pred_arr.round(), average=None)
+print("recall_score nè:", rc)
+ps = precision_score(real_rate_arr, pred_arr.round(), average=None)
+print("precision_score nè:", ps)
 '''print("rate_train test: ", rate_train)
 print("rate_train.shape", rate_train.shape)
 print("rate test: ", rate_test)
